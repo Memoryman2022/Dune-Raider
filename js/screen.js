6e679d08d556a79c5818db0852f0a551b947646a
Memoryman2022 <rlacannon@gmail.com>
2024-03-01 02:59:47 +0100

    music

--- js/screen.js
+++ js/screen.js
@@ -8,9 +8,13 @@ window.onload = function () {
   startButton.addEventListener("click", function () {
     const game = new Game(startScreen, gameEndScreen);
     game.start();
+    backgroundMusic.play();
   });
   restartButton.addEventListener("click", function () {
     const game = new Game(startScreen, gameEndScreen);
     game.restart();
   });
 };
+const backgroundMusic = new Audio("sounds2/dune_raider.mp3");
+backgroundMusic.volume = 0.5;
+backgroundMusic.loop = true;
